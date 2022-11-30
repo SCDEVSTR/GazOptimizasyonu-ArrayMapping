b321270a2a67ec8e83aa83443f31ef51cccdf407
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-11-30 15:59:58 +0300

    Update Optimization.sol

--- Optimization.sol
+++ Optimization.sol
@@ -2,22 +2,16 @@
 pragma solidity ^0.8.15;
 
 contract Optimization {
-//uint[] public array;
-//mapping(address => uint) public balances;
 
-/*
 uint[] public array;
 function usearray(uint item) public  {
    array.push(item);
    }
 
-*/
-/*
 mapping(uint => uint) public balances;
 
 function addClass(uint x, uint y) public {
          balances[x]=y;
 }
-*/
-   
+
 }
