4c4572eb82ea9f2b012079ec9302aebc15c46d79
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-11-30 15:57:52 +0300

    Create Optimization.sol

---
+++ Optimization.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+contract Optimization {
+//uint[] public array;
+//mapping(address => uint) public balances;
+
+/*
+uint[] public array;
+function usearray(uint item) public  {
+   array.push(item);
+   }
+
+*/
+/*
+mapping(uint => uint) public balances;
+
+function addClass(uint x, uint y) public {
+         balances[x]=y;
+}
+*/
+   
+}
